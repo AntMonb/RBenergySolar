db8d261baf868874f3510882961a02b887e38406
ANTONIO MONTES BURGOS <joseamontes1@unisinu.edu.co>
2025-05-21 12:53:53 -0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+// next.config.ts
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ya no uses experimental.appDir
+  // solo asegúrate de no tener errores
+};
+
+export default nextConfig;
